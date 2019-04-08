32b4e6ce9bc23eef67d5ef1a2f922b16e0bb4529
Kathleen pearce <kathleenpearce@live.com>
2019-04-08 23:59:37 +0000

    added secret stuff

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,28 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors"
+    headers: {
+      'User Agent': 'request',
+      'Authorization': 'Bearer' df0fa291d1937632e9f8db23a4cc963a18b36838
+    }
+
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
-}
+
+
+
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
