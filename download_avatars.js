74f3c08acaad20b74bd60688ac47e05f5c64df9d
Kathleen pearce <kathleenpearce@live.com>
2019-04-09 01:49:45 +0000

    added in the code to make images from each url user

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var token = require('./secret').GITHUB_TOKEN;
-console.log('Welcome to the GitHub Avatar Downloader!');
+//console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
@@ -11,18 +11,32 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
 };
   request(options, function(err, res, body) {
-      for (user in JSON.parse(body)) {
-        cb(err, JSON.parse(body)[user].avatar_url);
-      };
+    cb(err, JSON.parse(body))
   });
 }
 
 function downloadImageByURL(url, filePath) {
+  var request = require('request');
+  var fs = require('fs');
 
+request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Content Type ', response.headers['content-type'])
+       })
+       .pipe(fs.createWriteStream(filePath));
 }
-
+// downloadImageByURL('https://avatars0.githubusercontent.com/u/1615?v=4', './images/a.jpg');
+// downloadImageByURL(url, path)
 getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log(result);
+    for (let user of result) {
+      console.log(user.login)
+      console.log(user.avatar_url)
+      downloadImageByURL(user.avatar_url, './images/' + user.login + '.jpg' )
+    }
 
 });
 
